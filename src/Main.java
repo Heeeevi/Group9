37899d88fcbd876e64b7a01c96c542d538a63e44
Heeeevi <107466972+Heeeevi@users.noreply.github.com>
2024-06-16 20:32:09 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Menjalankan aplikasi GUI di Event Dispatch Thread
+        // Menjalankan aplikasi GUI
         SwingUtilities.invokeLater(() -> new MainWindow());
     }
 }
